be92c7c482550392cb244b3c22ef81f3cfb46104
pinballtec <daniilboiko99@gmail.com>
2022-03-24 16:58:11 +0100

    Video output is configured

--- video/static/js/streams.js
+++ video/static/js/streams.js
@@ -2,5 +2,38 @@ const APP_ID = ''
 const CHANNEL = ''
 const TOKEN = ''
 
+let UID;
+
 console.log('Agora is connected')
 
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+    document.getElementById('room-name').innerText = CHANNEL
+
+    try{
+        UID = await client.join(APP_ID, CHANNEL, TOKEN, UID)
+    }catch(error){
+        console.error(error)
+        window.open('/', '_self')
+    }
+    
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+
+    let player = `<div  class="video-container" id="user-container-${UID}">
+                     <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                     <div class="video-player" id="user-${UID}"></div>
+                  </div>`
+    
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+joinAndDisplayLocalStream()
